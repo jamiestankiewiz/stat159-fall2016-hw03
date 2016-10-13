f5b06b866e3a16502be0273e717ee14a645e5b63
Jamie <jamie.stankiewiz@berkeley.edu>
2016-10-13 14:02:38 -0700

    fixed location/paths, so now the makefile will run correctly on the relative path to the makefile

--- code/scripts/eda-script.R
+++ code/scripts/eda-script.R
@@ -1,5 +1,5 @@
 #setwd('C:/Users/Jamie/Documents/pandoc-test/stat159/stat159-fall2016-hw03/data/scripts')
-ad <- read.csv('../../data/Advertising.csv')
+ad <- read.csv('data/Advertising.csv')
 
 #Summary of TV
 summary(ad$TV)
@@ -14,19 +14,19 @@ summary(ad$Newspaper)
 summary(ad$Sales)
 
 #generating the histograms as images
-png("../../images/histogram-tv.png")
+png("images/histogram-tv.png")
 hist(ad$TV, breaks=15, xlab='TV', main='Histogram of TV Advertisement', ylim=c(0,20))
 dev.off()
 
-png("../../images/histogram-radio.png")
+png("images/histogram-radio.png")
 hist(ad$Radio, breaks=15, xlab='Radio', main='Frequency of Radio')
 dev.off()
 
-png("../../images/histogram-newspaper.png")
+png("images/histogram-newspaper.png")
 hist(ad$Newspaper, breaks=15, xlab='Newspaper', main='Frequency of Newspaper', ylim=c(0,50))
 dev.off()
 
-png("../../images/histogram-sales.png")
+png("images/histogram-sales.png")
 hist(ad$Sales, breaks=15, xlab='Sales', main='Frequency of Sales', ylim=c(0,50))
 dev.off()
 
@@ -35,12 +35,12 @@ corrmat <- round(cor(ad[2:5]),4)
 corrmat[lower.tri(corrmat)] <- NA
 
 #generating pairwise scatterplots
-png("../../images/scatterplot-matrix.png")
+png("images/scatterplot-matrix.png")
 plot(ad[2:5])
 dev.off()
 
 #creating the txt document
-sink('../../data/eda-output.txt')
+sink('data/eda-output.txt')
 
 print('Summary statistics of TV')
 summary(ad$TV)
@@ -60,4 +60,4 @@ corrmat
 sink()
 
 
-save(corrmat, file='../../data/correlation-matrix.RData')
+save(corrmat, file='data/correlation-matrix.RData')
--- code/scripts/regression-script.R
+++ code/scripts/regression-script.R
@@ -1,42 +1,42 @@
 #setwd('C:/Users/Jamie/Documents/pandoc-test/stat159/stat159-fall2016-hw03/data/scripts')
-ad <- read.csv('../../data/Advertising.csv')
+ad <- read.csv('data/Advertising.csv')
 
 regression <- lm(ad)
 regsum  <- summary(regression)
 
 #generating the RData
-save(regression, regsum, file='../../data/regression.RData')
+save(regression, regsum, file='data/regression.RData')
 
 #produces the residual plot, qqplot and scale-location plot
 plot(regression, which=c(1:3))
 
 
-png("../../images/residual-plot.png")
+png("images/residual-plot.png")
 plot(regression,which=1)
 dev.off()
 
-png("../../images/scale-location-plot.png")
+png("images/scale-location-plot.png")
 plot(regression, which =2)
 dev.off()
 
-png("../../images/normal-qq-plot.png")
+png("images/normal-qq-plot.png")
 plot(regression, which=3)
 dev.off()
 
 #individual scatterplots
-png("../../images/scatterplot-tv-sales.png")
+png("images/scatterplot-tv-sales.png")
 plot(ad$TV, ad$Sales, main='TV vs Sales', xlab= 'TV', ylab='Sales')
 lm1 <- lm(Sales ~ TV, data=ad)
 abline(lm1, col='red')
 dev.off()
 
-pdf("../../images/scatterplot-radio-sales.pdf")
+png("images/scatterplot-radio-sales.png")
 plot(ad$Radio, ad$Sales, main='Radio vs Sales', xlab= 'Radio', ylab='Sales')
 lm2 <- lm(Sales ~ Radio, data=ad)
 abline(lm2, col='red')
 dev.off()
 
-pdf('../../images/scatterplot-newspaper-sales.png')
+png('images/scatterplot-newspaper-sales.png')
 plot(ad$Newspaper, ad$Sales, main='Newspaper vs Sales', xlab= 'Radio', ylab='Sales')
 lm3 <- lm(Sales ~ Newspaper, data=ad)
 abline(lm3, col='red')
